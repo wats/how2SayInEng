89f1ebb0f7e00bd03561d970138869de8a574469
wats <arithinwonderland@gmail.com>
2013-12-14 15:20:49 +0900

    change to background script from page

---
+++ background.js
@@ -0,0 +1,12 @@
+function getClickHandler() {
+    return function(info, tab) {
+        alert("クリックされました");
+    };
+};
+
+var parentId = chrome.contextMenus.create({
+    "title" : "how2SayInEng",
+    "type" : "normal",
+    "contexts" : ["all"],
+    "onclick" : getClickHandler
+});
